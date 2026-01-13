dddc7c60d38cfc7e3181bb1f5f7e28c6a041f0b5
Виолетта <viol.gerasimova2015@yandex.ru>
2026-01-13 22:14:35 +0400

    feat(api): include keywords router

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from app.api.sources import router as sources_router
+from app.api.keywords import router as keywords_router
 
 api_router = APIRouter()
 api_router.include_router(sources_router)
+api_router.include_router(keywords_router)
+
