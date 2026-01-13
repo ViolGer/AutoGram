3ac36546b5b55fba177b56b423d520040ca39bca
Виолетта <viol.gerasimova2015@yandex.ru>
2026-01-13 20:42:57 +0400

    fix(api): include api router in app.main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 
+from app.api.router import api_router
 from app.db.base import create_db_and_tables
 
 app = FastAPI(title="AutoGram", version="0.0.1")
 
 
-@app.on_event('startup')
+@app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+
+app.include_router(api_router)
 
 
 @app.get("/health")
